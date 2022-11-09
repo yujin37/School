f08899c7a37a76deb093fc77b933c00bd3795299
yujinni <86652449+yujin37@users.noreply.github.com>
2022-11-09 15:24:09 +0900

    solution update

---
+++ Algorithm/leetcode_1971.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        p=[0]*(n)
+
+        def make_set(x):
+            p[x]=x
+            
+        def union(x,y):
+            p[find_set(y)]=find_set(x)
+        
+        def find_set(x):
+            if x==p[x]:
+                return x
+            else:
+                return find_set(p[x])
+
+        for i in range(n):
+            make_set(i)
+            
+        for i in edges:
+            union(i[0], i[1])
+        print(edges)
+        if find_set(source)==find_set(destination):
+            return True
+        else:
+            return False
+            
